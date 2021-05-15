0258c0e45d4c365a24b9047b8bdf763a4aea0136
Alexandra Kustova <avkustova@inbox.ru>
2021-05-15 20:06:46 +0300

    First done

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+from typing import List
+
 cook_book = {}
 key_ = ['ingredient_name', 'quantity', 'measure']
 
 with open('recipes.txt', 'r', encoding='utf-8') as f:
-    for i, line in enumerate(f):
-        if i == 0:
-            dish = line.strip()
-        elif i == 1:
-            count_ing = line.strip()
-    print(dish)
-    print(count_ing)
-    ingred_list = []
+    for line in f:
+        dish = line.strip()
+        counter = int(f.readline().strip())
+        keys_ = []
+        for _ in range(int(counter)):
+            q = f.readline().strip().split('|')
+            dish_dict = dict(zip(key_, q))
+            keys_ += [dish_dict]
+        cook_book[dish] = keys_
+        f.readline().strip()
+    print(cook_book)
+
+
+
+        # print(dish)
+
 
 
 
