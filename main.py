1b0a6c50bd008b4c6b66b4c46d9d2b73e2894c94
Alexandra Kustova <avkustova@inbox.ru>
2021-05-24 00:57:39 +0300

    Edit

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from typing import List
-
 cook_book = {}
-key_ = ['ingredient_name', 'quantity', 'measure']
 
-with open('recipes.txt', 'r', encoding='utf-8') as f:
+with open('recipes.txt', encoding='utf-8') as f:
     for line in f:
         dish = line.strip()
         counter = int(f.readline().strip())
-        keys_ = []
-        for _ in range(int(counter)):
-            q = f.readline().strip().split('|')
-            dish_dict = dict(zip(key_, q))
-            keys_ += [dish_dict]
-        cook_book[dish] = keys_
+        list_of_ingred = []
+        for i in range(counter):
+            temp_dict = {}
+            ingred = f.readline()
+            ingred = ingred.strip().split('|')
+            temp_dict['ingredient_name'] = ingred[0].strip()
+            temp_dict['quantity'] = int(ingred[1])
+            temp_dict['measure'] = ingred[2].strip()
+            list_of_ingred.append(temp_dict)
+        cook_book[dish]= list_of_ingred
         f.readline().strip()
     print(cook_book)
 
@@ -27,16 +28,14 @@ def get_shop_list_by_dishes(dishes, person_count, cook_dict):
                 items = ['measure', 'quantity']
                 for item in items:
                     one_dict[item] = ing[item]
-
-                one_dict["quantity"] = [x * person_count for x in one_dict['quantity']]
-
+                one_dict["quantity"] = person_count * one_dict['quantity']
                 new_dict.update({new_key: one_dict})
     print(new_dict)
 
 
         # print(cook_book[dishes])
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 3, cook_book)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 10, cook_book)
 
 
 
